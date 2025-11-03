8b22db09346c3fdb447b13aeac950b0c61dbd081
yhuet <yann@huet.dev>
2025-11-03 20:29:59 +0100

    Database

--- handler_user.go
+++ handler_user.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/yhuet/aggregator/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return errors.New("missing argument username")
 	}
-	err := s.conf.SetUser(cmd.args[0])
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return err
+	}
+	err = s.conf.SetUser(user.Name)
 	if err != nil {
 		return err
 	}
 	fmt.Printf("user set to: %s\n", cmd.args[0])
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("missing argument username")
+	}
+	currentTime := time.Now()
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: currentTime,
+		UpdatedAt: currentTime,
+		Name:      cmd.args[0],
+	})
+	if err != nil {
+		return err
+	}
+	err = s.conf.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("user created: %s\n", user.Name)
+	fmt.Println(user)
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.ResetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+	fmt.Println("users have been reset.")
 	return nil
 }
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
 	"github.com/yhuet/aggregator/internal/config"
+	"github.com/yhuet/aggregator/internal/database"
 )
 
 type state struct {
 	conf *config.Config
+	db   *database.Queries
 }
 
 func main() {
@@ -17,13 +21,22 @@ func main() {
 	}
 
 	commands.register("login", handlerLogin)
+	commands.register("register", handlerRegister)
+	commands.register("reset", handlerReset)
 
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error opening sql: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
 	state := state{
 		conf: &cfg,
+		db:   dbQueries,
 	}
 
 	args := os.Args
